a207471388202d32ae533e260a8fccdfb1bd12f9
edenn123 <nnaemeka.ede22@gmail.com>
2014-02-09 16:18:55 -0600

    Select function implementation

--- TableClass1.cpp
+++ TableClass1.cpp
@@ -1,29 +1,58 @@
-// TableClass1.cpp : Defines the entry point for the console application.
+// Tableclass1.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include <string>
+#include <vector>
+#include "attribute.h"
 #include "Table.h"
-
-int main(int argc, _TCHAR* argv[])
+using namespace std;
+void select(Table table,Attribute attribute,string findthis);
+int _tmain(int argc, _TCHAR* argv[])
 {
-
 	Attribute user_id("user_id", "int");
 	Attribute user_name("user_name", "string");
 	Attribute privilege_level("privilege_level", "string");
 
-	vector<Attribute> attributeList = { user_id, user_name, privilege_level };
-
+	vector<Attribute> attributeList;
+	attributeList.push_back(user_id);
+	attributeList.push_back(user_name);
+	attributeList.push_back(privilege_level);
 	Table table(attributeList);
-
-	vector<string> newRow = {"0","Bobby","admin"};
-	vector<string> newer = { "1", "Will", "guest" };
-
+	vector<string> newRow ;
+	newRow.push_back("0");
+	newRow.push_back("Bobby");
+	newRow.push_back("admin");
+	vector<string> newer;
+	newer.push_back("1");
+	newer.push_back("Will");
+	newer.push_back("guess");
+	string privilege="Will";
 	table.pushBackRow(newRow);
 	table.pushBackRow(newer);
 	table.printTable(table);
-
+	select(table,privilege_level,privilege);
+	system("pause");
 	return 0;
 }
+void select(Table table,Attribute attribute,string findthis)
+{
+  int i=0;
+  int q=0;
+  for(int j=0;j<table.attributes.size();j++)
+  {
+	if(table.attributes[j].name==attribute.name&&table.attributes[j].type==attribute.type)
+	{
+        q=j;	
+	}
+  }
+  while(i<table.rows.size())
+  {
 
-
-
+	  if(table.rows[i][q]==findthis)
+	  {
+      table.printRow(table.rows[i]);
+	  }
+	  i++;
+  }
+}
